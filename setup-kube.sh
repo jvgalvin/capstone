2c4255702485c778018738dc55aa0e3223becc94
Charles Martin S. Lim Jr <charlesmartin.lim@gmail.com>
2023-11-29 16:09:05 +0000

    updated scripts

--- setup-kube.sh
+++ setup-kube.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start minikube
-minikube start --driver=docker --force
+minikube start --driver=docker
 
 # Remove existing minikube images
 minikube image rm landmund/fastapi:20231120
