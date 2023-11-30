373ee553cd860f2704d66896386844d76c2f1b7e
Paul Arellano <paul590@gmail.com>
2023-11-30 00:45:18 -0500

    Added UI fixes

--- ui/alzheimer-predict-ui/src/inputs/ResultInput.js
+++ ui/alzheimer-predict-ui/src/inputs/ResultInput.js
@@ -2,7 +2,7 @@ function ResultInput({resultValue}) {
     return (
       <label>
         AD Probability
-        <input className="input-data" type="number" ref={resultValue} readOnly={true}></input>
+        <input className="result-input-data" type="number" ref={resultValue} readOnly={true}></input>
       </label>
     )
   }
--- ui/alzheimer-predict-ui/src/inputs/TextNumberInput.js
+++ ui/alzheimer-predict-ui/src/inputs/TextNumberInput.js
@@ -1,8 +1,14 @@
 function TextNumberInput({input_type = "N", name = "", input_text = "", handleChange, value}) {
   return (
     <label>
-      {input_text}
-      {input_type === "N" ? <input className="input-data" name={name} min="0" max="150" step="any" type="number" ref={value} onChange={handleChange} /> : <input className="input-data" name={name} maxLength="3" type="text" ref={value} onChange={handleChange} />}
+      <div class="d-flex flex-row mb-1">
+          <div className="input-label">
+            {input_text}
+          </div>
+          <div className="input-container">
+            {input_type === "N" ? <input className="input-data" name={name} min="0" max="150" step="any" type="number" ref={value} onChange={handleChange} /> : <input className="input-data" name={name} maxLength="3" type="text" ref={value} onChange={handleChange} />}
+          </div>
+        </div>
     </label>
   )
 }
