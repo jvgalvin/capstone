8f9d858602475080788fcb38817ea75cca51892d
Charles Martin S. Lim Jr <charlesmartin.lim@gmail.com>
2023-12-03 19:11:02 +0800

    added to scripts

--- kube-delete.sh
+++ kube-delete.sh
@@ -3,6 +3,8 @@ minikube image rm landmund/fastapi:20231120
 minikube image rm landmund/postgres:20231120
 minikube image rm landmund/nodejs:20231120
 
+sleep 5
+
 # Remove local images
 docker rmi landmund/fastapi:20231120
 docker rmi landmund/postgres:20231120
